1089e2e17b1788c48e446341b9a7d3d882c433a6
Yosuke Tamura <tamura.yosuke.tp8@gmail.com>
2015-10-27 19:20:33 +0900

    カメラキャプチャプログラム追加

---
+++ src/PhotoCapture.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from naoqi import ALProxy
+
+photo = ALProxy("ALPhotoCapture", "163.221.68.225", 9559)
+photo.setPictureFormat("jpeg")
+photo.setResolution(2)
+photo.takePicture("/home/nao/recordings/cameras/", "image")
