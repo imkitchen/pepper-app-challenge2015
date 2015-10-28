ef84f397104f651d608eb60cb56477bee9a107b4
Yosuke Tamura <tamura.yosuke.tp8@gmail.com>
2015-10-28 14:20:13 +0900

    classify

--- src/photo_capture.py
+++ src/photo_capture.py
@@ -3,7 +3,13 @@
 
 from naoqi import ALProxy
 
-photo = ALProxy("ALPhotoCapture", "163.221.68.225", 9559)
-photo.setPictureFormat("jpeg")
-photo.setResolution(2)
-photo.takePicture("/home/nao/recordings/cameras/", "image")
+class PhotoCapture:
+
+    def __init__(self, app):
+        self.app = app
+        self.photo = app.proxy["ALPhotoCapture"]
+
+    def capture(self):
+        self.photo.setPictureFormat("jpeg")
+        self.photo.setResolution(2)
+        self.photo.takePicture("/home/nao/recordings/cameras/", "image")
