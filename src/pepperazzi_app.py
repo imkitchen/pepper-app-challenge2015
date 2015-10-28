d17754f6cfa360da520af1223d9c8a88351d7c8c
Yosuke Tamura <tamura.yosuke.tp8@gmail.com>
2015-10-28 13:17:52 +0900

    コマンドラインオプションでIPとPortを指定するようにした

--- src/pepperazzi_app.py
+++ src/pepperazzi_app.py
@@ -1,21 +1,51 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from naoqi import ALProxy
-#from pepperazzi import
+import sys
+import logging
+from argparse import ArgumentParser
+from naoqi    import ALProxy
 
 class PepperazziApp:
-    def __init__(self, config):
-        self.config = config
-        #self.image_uploader = ImageUploader()
+    def __init__(self, ip_addr, port):
+        self.ip_addr = ip_addr
+        self.port    = port
+        self.proxy   = []
 
-    def use(self, proxies):
-        [ self.proxy. ALProxy(proxy) for proxy in proxies ]
+    def inject(self, proxies):
+        for proxy in proxies:
+            self.proxy[proxy] = ALProxy(proxy, self.ip_addr, self.port)
                 
     def run(self):
-        return
+        print("pepper IP adress: " + self.ip_addr)
+        print("port number: " + self.port)
         
-#######################################
+    @staticmethod
+    def main():
+        parser = ArgumentParser()
 
+        parser.add_argument("-i", "--ip_addr",
+                            dest    = "ip_addr",
+                            type    = str,
+                            default = "127.0.0.1",
+                            help    = "PepperのIPアドレス")
+        
+        parser.add_argument("-p", "--port",
+                            dest    = "port",
+                            type    = int,
+                            default = 9559,
+                            help    = "ポート番号")
+        
+        args = parser.parse_args()
+        app  = PepperazziApp(args.ip_addr, args.port)
+        app.inject([
+            "ALMotion",
+            "ALTextToSpeech",
+            "ALPhotoCapture",
+        ])
+        app.run()
+
+#######################################
+    
 if __name__ == "__main__":
-    app = PepperazziApp(ip, port)
-    app.run()
+    PepperazziApp.main()
