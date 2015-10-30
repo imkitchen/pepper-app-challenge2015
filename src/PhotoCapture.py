ca107a4a638ad8216431327fe0423d7d20999670
NaokiMaeda <maeda.naoki.md9@is.naist.jp>
2015-10-30 16:41:55 +0900

    change photo resolution

--- src/PhotoCapture.py
+++ src/PhotoCapture.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-
+import datetime
 from naoqi import ALProxy
 
-photo = ALProxy("ALPhotoCapture", "163.221.68.225", 9559)
+photo = ALProxy("ALPhotoCapture", "192.168.10.74", 9559)
 photo.setPictureFormat("jpeg")
-photo.setResolution(2)
-photo.takePicture("/home/nao/recordings/cameras/", "image")
+photo.setResolution(4)
+photo.takePicture("/home/nao/recordings/cameras/", str(datetime.datetime.today()))
